a0e881a36bf1713d0fdab51f317ce2a2948cd989
vadim <vadim.liaschko123@gmail.com>
2024-06-06 12:08:11 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+import path from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+const targets = browserslistToTargets(browserslist('>=0.25%'));
+
+export default {
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: targets,
+      options: {
+        customProperties: true,
+        minify: true,
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: path.resolve(__dirname, 'src/partials'),
+      context: {
+        title: 'Hello, world!',
+      },
+    }),
+    // Удалено: дублирование lightningcss
+  ],
+  build: {
+    target: targets,
+  },
+};
